4007a7715931404e8b58182aa704bd4961eb17fc
Jeremy Bernard <jeremy.bernard@cpexterne.org>
2019-08-26 15:44:24 +0200

    Cleanup Canvas componant

--- src/componants/Canvas.jsx
+++ src/componants/Canvas.jsx
@@ -3,34 +3,45 @@ import './css/Canvas.css'
 import Tileset from '../graphic/Tileset'
 import { randomInt } from '../tools/random'
 
-const Canvas = (props) => {
-  let locations = []
-  const canvasRef = React.createRef()
-
-  const groundTileset = new Tileset('/assets/tilesets', 'ground.png', 512, 384, 32, 32)
-
-  const handleCanvasClick = (e) => {
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
-    const rect = canvas.getBoundingClientRect()
-    const x = e.clientX - rect.left
-    const y = e.clientY - rect.top
-    const id = randomInt(1, groundTileset.tileCount)
-    const newLocation = { id, x, y }
-
-    locations = [ ...locations, newLocation ]
-    ctx.clearRect(0, 0, props.height, props.width)
-    locations.forEach(location => groundTileset.drawTile(location.id, ctx, location.x, location.y))
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef()
+    this.canvas = null
+    this.context = null
   }
 
-  return (
-    <canvas
-      ref={ canvasRef }
-      width={ props.width }
-      height={ props.height }
-      onMouseDown={ handleCanvasClick }
-    />
-  )
+  componentDidMount() {
+    this.canvas = this.canvasRef.current
+    this.context = this.canvas.getContext('2d')
+  }
+
+  render() {
+    let locations = []
+
+    const groundTileset = new Tileset('/assets/tilesets', 'ground.png', 512, 384, 32, 32)
+
+    const handleCanvasClick = (e) => {
+      const rect = this.canvas.getBoundingClientRect()
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
+      const id = randomInt(1, groundTileset.tileCount)
+      const newLocation = { id, x, y }
+
+      locations = [ ...locations, newLocation ]
+      this.context.clearRect(0, 0, this.props.height, this.props.width)
+      locations.forEach(location => groundTileset.drawTile(location.id, this.context, location.x, location.y))
+    }
+
+    return (
+      <canvas
+        ref={ this.canvasRef }
+        width={ this.props.width }
+        height={ this.props.height }
+        onMouseDown={ handleCanvasClick }
+      />
+    )
+  }
 }
 
 export default Canvas;
